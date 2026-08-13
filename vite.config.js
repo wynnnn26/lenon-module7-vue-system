8e9118420232f3b01e0b47333c7942dc3816786e
Kerwynn Lenon <jandel1611@gmail.com>
2026-08-13 21:21:36 +0800

    Configure Tailwind CSS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [vue()],
+ plugins: [vue(), tailwindcss()],
 })
